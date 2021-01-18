21608f842c41e52065d9110dcab425c85830f8a2
jiwansvmx <jiwan.bhandari@servicemax.com>
2021-01-17 16:10:34 -0800

    fixed

--- lib.h
+++ lib.h
@@ -21,11 +21,10 @@ class hm {
 private:
   template <typename k = key> size_t index(const k &k_, size_t s) {
     size_t h = hash()(k_);
-    fmt::print("hash {} for key {} index {}\n", h, k_, h % s);
     return h % s;
   }
   template <typename k = key>
-  size_t probe(size_t i, k &&k_, container_type &nodes_) {
+  [[nodiscard]] size_t probe(size_t i, k &&k_, container_type &nodes_) {
     for (size_t j = i; j < nodes_.capacity(); ++j) {
       if (!nodes_[j] || nodes_[j].value().first == k_) {
         return i + (j - i);
@@ -41,7 +40,6 @@ private:
 
   template <typename... Args>
   void emplace_at(element_type *elem_ptr, Args &&...args) {
-    fmt::print("caling emplace args with variadic\n");
     elem_ptr->~element_type();
     new (elem_ptr) element_type(std::in_place, std::forward<Args>(args)...);
   }
@@ -49,15 +47,12 @@ private:
   void resize(size_t new_size) {
     container_type new_m_nodes;
     new_m_nodes.reserve(new_size);
-    fmt::print(" resizing to new size {}\n", new_size);
     for (size_t i = 0; i < m_nodes.capacity(); ++i) {
       auto &node = m_nodes[i];
       if (node) {
         auto &k_ = node.value().first;
-        fmt::print(stderr, "key = {} index={}\n", k_,
-                   index(k_, new_m_nodes.capacity()));
-        auto idx = probe(index(k_, new_m_nodes.capacity()), k_, new_m_nodes);
-        emplace_at(&m_nodes[i], std::move(node));
+        size_t idx = probe(index(k_, new_m_nodes.capacity()), k_, new_m_nodes);
+        emplace_at(&new_m_nodes[idx], std::move(node));
       }
     }
     m_nodes = std::move(new_m_nodes);
@@ -82,7 +77,7 @@ public:
   auto size() const { return m_count; }
 
   template <typename k = key> bool exists(const k &&k_) {
-    auto idx = probe(index(k_, m_nodes.capacity()), k_, m_nodes);
+    size_t idx = probe(index(k_, m_nodes.capacity()), k_, m_nodes);
     if (idx >= m_nodes.capacity() || !m_nodes[idx]) {
       return false;
     }
@@ -90,18 +85,14 @@ public:
   }
 
   template <class k = key, class v = value> void insert(k &&k_, v &&v_) {
-
     auto load_factor = static_cast<double>(m_count + 1) /
                        static_cast<double>(m_nodes.capacity());
     if (load_factor > expected_load_factor) {
       resize(m_nodes.capacity() * 2);
     }
-    // fmt::print("size{} load_factor={}\n", m_nodes.size(), load_factor);
     auto idx = probe(index(k_, m_nodes.capacity()), k_, m_nodes);
-    // fmt::print("position =  {}\n", idx);
     emplace_at(&m_nodes[idx], std::forward<k>(k_), std::forward<v>(v_));
     m_count += 1;
-    fmt::print("{}\n", *this);
   }
 };
 
--- libtest.cpp
+++ libtest.cpp
@@ -1,3 +1,5 @@
+#include <random>
+
 #include "fmt/core.h"
 #include "fmt/ostream.h"
 #include "gtest/gtest.h"
@@ -19,16 +21,41 @@ TEST(hm, Insert) {
 
 TEST(hm, PreservesOnResize) {
   lite::hm<int, int> hm_{};
-  hm_.insert(20, 20);
   hm_.insert(10, 10);
+  hm_.insert(20, 20);
   hm_.insert(30, 30);
   hm_.insert(40, 40);
   hm_.insert(50, 50);
   hm_.insert(60, 60);
   hm_.insert(70, 70);
   hm_.insert(80, 80);
-  hm_.insert(10, 11);
-  fmt::print(stderr, "printing the hashmap {}\n", hm_);
-  // ASSERT_EQ(hm_.exists(40), true);
-  // ASSERT_EQ(hm_.exists(70), false);
+  hm_.insert(90, 90);
+  hm_.insert(100, 100);
+  hm_.insert(110, 110);
+  hm_.insert(120, 120);
+  hm_.insert(130, 130);
+  hm_.insert(140, 140);
+  fmt::print("{}\n", hm_);
+  ASSERT_EQ(hm_.exists(10), true);
+  ASSERT_EQ(hm_.exists(20), true);
+  ASSERT_EQ(hm_.exists(30), true);
+  ASSERT_EQ(hm_.exists(40), true);
+  ASSERT_EQ(hm_.exists(50), true);
+  ASSERT_EQ(hm_.exists(60), true);
+  ASSERT_EQ(hm_.exists(70), true);
+  ASSERT_EQ(hm_.exists(80), true);
+  ASSERT_EQ(hm_.exists(90), true);
+  ASSERT_EQ(hm_.exists(100), true);
+  ASSERT_EQ(hm_.exists(110), true);
+  ASSERT_EQ(hm_.exists(120), true);
+  ASSERT_EQ(hm_.exists(130), true);
+  ASSERT_EQ(hm_.exists(140), true);
+  fmt::print("-----------------\n");
+  std::mt19937 gen;
+  // seed the engine with an unsigned int
+  gen.seed(11);
+  std::uniform_int_distribution<> distrib(200, 300);
+  for (size_t i = 0; i < 40; ++i) {
+    ASSERT_EQ(hm_.exists(distrib(gen)), false);
+  }
 }
